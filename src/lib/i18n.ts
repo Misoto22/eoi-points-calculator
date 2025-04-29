837c011d56768a03c7c3462c75bb1a8df63bd41a
Misoto22 <cxw2234466599@gmail.com>
2025-04-30 02:19:29 +0800

    package.json updated

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -1,9 +1,33 @@
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { createInstance } from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import resourcesToBackend from 'i18next-resources-to-backend';
 
-export async function getStaticProps({ locale }: { locale: string }) {
+const initI18next = async (locale: string) => {
+  const i18nInstance = createInstance();
+  await i18nInstance
+    .use(initReactI18next)
+    .use(resourcesToBackend((language: string, namespace: string) => 
+      import(`../../public/locales/${language}/${namespace}.json`)))
+    .init({
+      lng: locale,
+      fallbackLng: 'en',
+      supportedLngs: ['en', 'zh'],
+      defaultNS: 'common',
+      fallbackNS: 'common',
+      interpolation: {
+        escapeValue: false,
+      },
+    });
+  return i18nInstance;
+};
+
+export async function getStaticProps({ locale = 'en' }) {
+  const i18n = await initI18next(locale);
   return {
     props: {
-      ...(await serverSideTranslations(locale, ['common'])),
+      messages: (await i18n.getResourceBundle(locale, 'common')),
     },
   };
-} 
+}
+
+export default initI18next; 
